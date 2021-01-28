3a9e90240d1955f3ebed99b675c077138c0ba775
PedroQGomes <pedromqgomes@gmail.com>
2021-01-28 17:52:46 +0000

    timeout to input

--- TP3/server.py
+++ TP3/server.py
@@ -1,4 +1,5 @@
 import socket
+import sys, select
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 12345))
@@ -9,9 +10,17 @@ while True:
     clientsocket, address = s.accept()
     print("Conneçao do cliente aceite")
 
-    print("Introduz o teu codigo")
-    input1 = (f"{input()}")
+    print("Introduz o teu codigo - tens 30 segundos")
 
-    clientsocket.send(bytes(f"{input1}", "utf-8"))
-    print("Dados enviados - Terminar Conneçao")
+    i, o, e = select.select( [sys.stdin], [], [], 30 )
+
+
+    if (i):
+        input1 = (f"{sys.stdin.readline().strip()}")
+        clientsocket.send(bytes(f"{input1}", "utf-8"))
+        print("Dados enviados - Terminar Conneçao")
+    else:
+        print("Erro: Expiraram os 30 segundos")
+
+
     clientsocket.close()
